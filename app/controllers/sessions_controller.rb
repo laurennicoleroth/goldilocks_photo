cd90a71912ab1336452245152070b16a2efc4713
Lauren Nicole Roth <laurenroth@gmail.com>
2015-07-13 17:18:02 -0400

    Add create and destroy session methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	user = User.find_by_email(params[:email])
+  	if user && user.authenticate(params[:password])
+  		#save user id in browser cookie. keeps user logged in.
+  		session[:user_id] = user.id
+  		redirect_to '/'
+  	end
   end
 
   def destroy
+  	session[:user_id] = nil
+  	redirect_to '/login'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+ # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-    # These routes will be for signup. The first renders a form in the browse, the second will 
-    # receive the form and create a user in our database using the data given to us by the user.
-    get '/signup' => 'users#new'
-    post '/users' => 'users#create'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # You can have the root of your site routed with "root"
   root 'users#new'
